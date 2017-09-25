0affac1adbcd03c0bb70d371b41626ea554a5165
dred-misha <32277055+dred-misha@users.noreply.github.com>
2017-09-25 22:19:07 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ТНС Энерго. Заполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "522042052809";
+             
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Счет клиента";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Дневной тариф";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ночной тариф";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 202);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.Threading;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //создаем переменную типа Iwebdriver
+        IWebDriver Browser;
+        //Адрес проживаниия
+        string Adr = "ул Премудрова, д. 10корп.1, кв.58";//потребуется для доп проверки по адресу
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // проверка на ввод данных в поля
+            if ((textBox1.Text != String.Empty) && (textBox2.Text != String.Empty) && (textBox3.Text != String.Empty))
+            {
+                //запускаем браузеа и заходим на страничку.
+                Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+                Browser.Navigate().GoToUrl("https://nn.tns-e.ru");
+                //ищем textbox по имени класса для ввода  и пихаем туда значение из нашего текстбокса
+                IWebElement SearchElement = Browser.FindElement(By.ClassName("ls-input"));
+                SearchElement.SendKeys(textBox1.Text);
+                //ищем кнопульку через css выражение и жмем на нее 
+                SearchElement = Browser.FindElement(By.CssSelector(".butt .btn"));
+                SearchElement.Click();
+                //проверка верности адреса проживания
+                //Надо доделать проверку на правильный адрес. Не получилось найти и сравнить выводимый адрес на страничке
+                //SearchElement = Browser.FindElement(By.ClassName("s-desc light cust-class-service-3"));
+                //if (SearchElement.Text == "ул Премудрова, д.   10корп.1, кв.58")
+                //{
+                //MessageBox.Show("Адрес верен");
+                // }
+                //подтверждаем адрес по нажатия 
+                Thread.Sleep(1000); //без задержки почему то не срабатывало
+                SearchElement = Browser.FindElement(By.CssSelector(".butt .btn"));
+                SearchElement.Click();
+
+                SearchElement = Browser.FindElement(By.Name("peak"));
+                SearchElement.SendKeys(textBox2.Text);
+                SearchElement = Browser.FindElement(By.Name("night"));
+                SearchElement.SendKeys(textBox3.Text);
+
+            }
+            else
+            {
+             MessageBox.Show("Необходимо ввести все данные!", "Ошибка ввода данных", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            //произвети обработку кнопки cancel
+            }
+        }
+    }
+}
